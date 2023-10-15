12110e1495fe405f63d54444796299c4fe657ce2
JuliaLiani <114131692+JuliaLiani@users.noreply.github.com>
2023-10-15 05:22:08 -0400

    added

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("keydown", function(e){
+    if(e.key === 'j'){
+        document.body.style.backgroundColor = '#555843';
+    }
+    else if(e.key === 'u'){
+        document.body.style.backgroundColor = '#D0D4CA';
+    }
+    else if(e.key === 'l'){
+        document.body.style.backgroundColor = '#A7D397';
+    }
+    else if(e.key === 'i'){
+        document.body.style.backgroundColor = '#F5EEC8';
+    }
+    else if(e.key === 'a'){
+        document.body.style.backgroundColor ='#F8BDEB';
+    }
+    else {
+        document.body.style.backgroundColor = '#FF8080';
+    }
+    }
+)
